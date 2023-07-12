8e37310712953c91556907aee0b98f9a500a2479
FMZPNP <paco.martinez@pnptc.com>
2023-07-12 19:14:28 +0200

    Validate Input data with data transfer objects

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,6 +1,6 @@
-export class UpdateCoffeeDto {
-    // we make all the properties optional to allow partial updates
-    readonly name?: string;
-    readonly brand?: string;
-    readonly flavors?: string[];
-}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCoffeeDto } from "./create-coffee.dto";
+
+//PartialType is a utility function that takes a class as an argument and returns a new class that behaves exactly
+// like the class passed in, except that all the properties are optional now.
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) { }
