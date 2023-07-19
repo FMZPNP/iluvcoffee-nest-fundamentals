84577b69ec3692a0866e4af9dbe455e68fcdbe55
FMZPNP <paco.martinez@pnptc.com>
2023-07-19 18:54:58 +0200

    TypeORM module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { type } from 'os';
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // change to your username
+      password: 'pass123', // change to your password
+      database: 'postgres', // change to your db name
+      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+      synchronize: true, // switch to false on prod
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
