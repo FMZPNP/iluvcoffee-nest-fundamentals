1b58ee806c2881d4c9ff3f010f056bc98f2e0cee
FMZPNP <paco.martinez@pnptc.com>
2023-10-06 16:45:25 +0200

    Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { type } from 'os';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env', // load this file
+    }), // load .env file
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres', // change to your username
-      password: 'pass123', // change to your password
-      database: 'postgres', // change to your db name
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER, // change to your username
+      password: process.env.DATABASE_PASSWORD, // change to your password
+      database: process.env.DATABASE_NAME, // change to your db name
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // switch to false on prod
     }),
