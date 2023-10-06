15aed94e67c743fce8e61e5ce90a277f69da54fe
FMZPNP <paco.martinez@pnptc.com>
2023-10-06 16:52:06 +0200

    Schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env', // load this file
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      })
     }), // load .env file
     CoffeesModule,
     TypeOrmModule.forRoot({
